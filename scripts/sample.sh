69cb16aaec81327e3d5dfaf48ba3cf4db847e745
akthar <akthar@qualcomm.com>
2018-11-21 21:54:58 +0530

    Folder Script Created and A Script Added

---
+++ scripts/sample.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+echo "What is your name?"
+read name
+echo "How do you do, $name?"
+read remark
+echo "i am $remark too!"
+echo "Do you go to school tomorrow?"
+read remark
+echo "Have a Good Day and Enjoy"
